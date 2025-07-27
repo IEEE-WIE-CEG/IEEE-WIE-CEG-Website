eff1e7100f9b5cc7377445032a3f10c73c023d1b
vjk-2k5 <svijaykrishna12@gmail.com>
2025-07-27 17:00:16 +0530

    removed mock articles

--- lib/articles-data.ts
+++ lib/articles-data.ts
@@ -13,35 +13,6 @@ export interface ArticleData {
   abstract?: string
 }
 
-import aiInHealthcare from "@/lib/articles-data/ai-in-healthcare"
-import womenInTech from "@/lib/articles-data/women-in-tech-leadership"
-
 export const articles: ArticleData[] = [
-  {
-    id: aiInHealthcare.id,
-    title: aiInHealthcare.title,
-    author: aiInHealthcare.author,
-    authorRole: aiInHealthcare.authorRole,
-    publishDate: aiInHealthcare.publishDate,
-    readTime: aiInHealthcare.readTime,
-    excerpt: aiInHealthcare.excerpt,
-    tags: aiInHealthcare.tags,
-    featuredImage: aiInHealthcare.featuredImage,
-    abstract: aiInHealthcare.abstract,
-  },
-  {
-    id: womenInTech.id,
-    title: womenInTech.title,
-    author: womenInTech.author,
-    authorRole: womenInTech.authorRole,
-    publishDate: womenInTech.publishDate,
-    readTime: womenInTech.readTime,
-    excerpt: womenInTech.excerpt,
-    tags: womenInTech.tags,
-    featuredImage: womenInTech.featuredImage,
-    abstract: womenInTech.abstract,
-  },
-  // Add more articles here as you create more files
+  // Your real research articles will be added here
 ]
-
-export { aiInHealthcare, womenInTech }
